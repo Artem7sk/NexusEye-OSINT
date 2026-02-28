8172ca14c831b3359686484ac9cad1546caee912
Artem7sk <92425901+Artem7sk@users.noreply.github.com>
2026-02-28 14:36:03 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+import os
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+from bot.handlers import router
+
+# Загружаем переменные из .env
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def main():
+    # Настраиваем логирование, чтобы видеть ошибки в консоли
+    logging.basicConfig(level=logging.INFO)
+    
+    if not TOKEN:
+        exit("Error: BOT_TOKEN not found in .env file")
+
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    
+    # Подключаем наши обработчики команд
+    dp.include_router(router)
+
+    print("👁 NexusEye OSINT System Online")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("System offline.")
