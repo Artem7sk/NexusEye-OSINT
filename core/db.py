c12abce1abc416faa9622c8bc062aaefce75f0c1
Artem7sk <92425901+Artem7sk@users.noreply.github.com>
2026-02-28 18:09:24 +0500

    Update db.py

--- core/db.py
+++ core/db.py
@@ -14,6 +14,15 @@ def init_db():
     conn.commit()
     conn.close()
 
+def get_stats():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    # Считаем общее количество юзеров и общее число запросов
+    cursor.execute('SELECT COUNT(*), SUM(requests_count) FROM users')
+    stats = cursor.fetchone()
+    conn.close()
+    return stats # Возвращает кортеж (кол-во_юзеров, кол-во_запросов)
+
 def add_user(user_id, username):
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
@@ -27,3 +36,26 @@ def update_request_count(user_id):
     cursor.execute('UPDATE users SET requests_count = requests_count + 1 WHERE user_id = ?', (user_id,))
     conn.commit()
     conn.close()
+
+def is_user_premium(user_id):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute('SELECT is_premium FROM users WHERE user_id = ?', (user_id,))
+    result = cursor.fetchone()
+    conn.close()
+    return result[0] if result else False
+def set_premium_status(user_id, status: bool):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute('UPDATE users SET is_premium = ? WHERE user_id = ?', (status, user_id))
+    conn.commit()
+    conn.close()
+
+def get_detailed_stats():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    # Считаем: всего юзеров, всего запросов, и сколько из них Premium
+    cursor.execute('SELECT COUNT(*), SUM(requests_count), (SELECT COUNT(*) FROM users WHERE is_premium = 1) FROM users')
+    stats = cursor.fetchone()
+    conn.close()
+    return stats # Возвращает (всего_юзеров, всего_запросов, премиум_юзеров)
